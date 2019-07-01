e3e53c67877fc91427b3a46965725c7b98caf20c
Narisoa Andriambololona <narisoa.andriambololona@axian-group.com>
2019-07-01 08:56:07 +0300

    rectification

--- emag-content/themes/e_magazine/inc/extends/custom-types/emagazine.php
+++ emag-content/themes/e_magazine/inc/extends/custom-types/emagazine.php
@@ -8,7 +8,6 @@
 define( 'TABLE_MAGAZINE', 'trace_magazine' );
 add_action('init', 'htds_init_post', 1);
 function htds_init_post(){
-    global $wpdb;
     //remove tags in post
     unregister_taxonomy_for_object_type( 'post_tag', 'post' );
     add_post_type_support('post','thumbnail', 'revisions');
@@ -20,25 +19,5 @@ function htds_init_post(){
     $labels->name_admin_bar = "Magazine";
     $labels->menu_name = "Magazine";
     $wp_post_types['post']->menu_icon = 'dashicons-book-alt';
-
-    //insert table
-    $trace_magazine_table = $wpdb->prefix . TABLE_MAGAZINE;
-    if($wpdb->get_var("show tables like '$trace_magazine_table'") != $trace_magazine_table){
-        $sql = "CREATE TABLE " . $trace_magazine_table . " (
-              `mag_id` INT (11) UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-              `mag_postid` INT (11) NOT NULL,
-              `mag_order` INT (11) NOT NULL,
-              `hub_id_sociaux` VARCHAR (225) NOT NULL,
-              `hub_image` VARCHAR (225) NOT NULL,
-              `hub_like` VARCHAR (225) NOT NULL,
-              `hub_comment` VARCHAR (225) NOT NULL,
-              `hub_timestamp` INT (11) NOT NULL
-            ) ;";
-
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql);
-
-    }
-
 
 }

658f2eb9914678362223466d431224e634f10772
felix <felix77@yahoo.fr>
2019-07-08 17:48:51 +0300

    page accueil

--- emag-content/themes/e_magazine/searchform.php
+++ emag-content/themes/e_magazine/searchform.php
@@ -9,7 +9,15 @@
  */
 ?>
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<label for="s" class="assistive-text"><?php _e( 'Search', 'e_magazine' ); ?></label>
-		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'e_magazine' ); ?>" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'e_magazine' ); ?>" />
+		<div class="search">
+			<label for="searchId" class="assistive-text"><?php _e( 'Search', 'e_magazine' ); ?></label>
+			<div class="input-group">
+				<input type="text" class="field form-control" name="searchId" id="searchId" placeholder="<?php esc_attr_e( 'Search', 'e_magazine' ); ?>" />
+				<div class="input-group-append">
+					<button class="btn btn-secondary" type="button">
+						<i class="fa fa-search"></i>
+					</button>
+				</div>
+		    </div>
+		</div>
 	</form>
